80946fc46ac4e771a76550556f229d888ca8209f
Barneet Panda <1905785@kiit.ac.in>
2023-03-09 23:07:56 +0530

    Test2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>This is My app </h1>
+        <h1>This is my test</h1>
+        <h1>This is my test2</h1>
       </header>
     </div>
   );
